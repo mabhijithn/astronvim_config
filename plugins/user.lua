277b2087b694f91444bd785eee63e8adbe4ecc5b
Abhijith Mundanad Narayanan <mabhijithn@fastmail.com>
2023-06-22 23:47:35 +0200

    Catppuccin theme added

--- plugins/user.lua
+++ plugins/user.lua
@@ -16,5 +16,6 @@ return {
   {
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.diagnostics.trouble-nvim" },
-  }
+  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
 }
